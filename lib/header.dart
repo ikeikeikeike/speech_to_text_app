a5858ca129e9fee3f4a202ab0f9d1bb76839e72f
ikeikeikeike <jp.ne.co.jp@gmail.com>
2019-07-02 02:39:11 +0900

    initial

---
+++ lib/header.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+//import 'dart:developer';
+
+enum TabsStyle { none }
+
+class _Page {
+  const _Page({this.icon, this.text});
+  final IconData icon;
+  final String text;
+}
+
+const List<_Page> _allPages = <_Page>[
+  _Page(icon: Icons.grade, text: 'TRIUMPH'),
+  _Page(icon: Icons.playlist_add, text: 'NOTE'),
+];
+
+class ScrollableTabs extends StatefulWidget {
+  static const String routeName = '/scrollable-tabs';
+
+  @override
+  ScrollableTabsState createState() => ScrollableTabsState();
+}
+
+class ScrollableTabsState extends State<ScrollableTabs>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(vsync: this, length: _allPages.length);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Speech To Text'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.sentiment_very_satisfied),
+            onPressed: () {
+              setState(() {});
+            },
+          ),
+        ],
+        bottom: TabBar(
+          controller: _controller,
+          isScrollable: true,
+          indicator: const UnderlineTabIndicator(),
+          tabs: _allPages.map<Tab>((_Page page) {
+            return Tab(text: page.text, icon: Icon(page.icon));
+          }).toList(),
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: _allPages.map<Widget>((_Page page) {
+          return SafeArea(
+            top: false,
+            bottom: false,
+            child: Container(
+              key: ObjectKey(page.icon),
+              padding: const EdgeInsets.all(12.0),
+              child: Card(
+                child: Center(
+                  child: Icon(
+                    page.icon,
+                    color: Theme.of(context).accentColor,
+                    size: 128.0,
+                    semanticLabel: 'Placeholder for ${page.text} tab',
+                  ),
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (int idx) {
+          setState(() {
+            currentIndex = idx;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.mail),
+            title: new Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.person),
+            title: new Text('Profile'),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+import 'app.dart';
+
+void main() {
+  enableFlutterDriverExtension();
+  runApp(App());
+}
